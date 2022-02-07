3346ec7bde0d6499b460abd1d31fa878b794e875
shakilhossain-cse <shakilhossainp.cse@gmail.com>
2022-02-07 16:37:07 +0600

    add page desin done

--- client/src/pages/Forget.jsx
+++ client/src/pages/Forget.jsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import { Box, Button, TextField, Typography } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Forget = () => {
-  return <div>Forget</div>;
+  const submitHandeler = () => {};
+  return (
+    <div>
+      <form onSubmit={submitHandeler}>
+        <Box
+          sx={{
+            maxWidth: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+            height: "100vh",
+            width: "100vw",
+          }}
+        >
+          <Typography variant="h3" gutterBottom component="div">
+            Forget Password
+          </Typography>
+          <Box sx={{ width: 500 }}>
+            <TextField
+              fullWidth
+              type="email"
+              label="Email"
+              id="fullWidth"
+              sx={{ marginBottom: "2rem" }}
+            />
+
+            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+              <Link
+                to="/login"
+                style={{ textDecoration: "none", marginBottom: "1rem" }}
+              >
+                login
+              </Link>
+              <Link
+                to="/register"
+                style={{ textDecoration: "none", marginBottom: "1rem" }}
+              >
+                register
+              </Link>
+            </Box>
+            <Button variant="contained" color="primary" fullWidth>
+              Get Link
+            </Button>
+          </Box>
+        </Box>
+      </form>
+    </div>
+  );
 };
 
 export default Forget;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,70 @@
-import React from 'react';
+import React from "react";
+import { Box, TextField, Typography, Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div>register</div>;
+  const submitHandeler = () => {};
+  return (
+    <div>
+      <form onSubmit={submitHandeler}>
+        <Box
+          sx={{
+            maxWidth: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+            height: "100vh",
+            width: "100vw",
+          }}
+        >
+          <Typography variant="h3" gutterBottom component="div">
+            Register
+          </Typography>
+          <Box sx={{ width: 500 }}>
+          <TextField
+              fullWidth
+              label="Name"
+              id="fullWidth"
+              sx={{ marginBottom: "2rem" }}
+            />
+            <TextField
+              fullWidth
+              label="Email"
+              id="fullWidth"
+              sx={{ marginBottom: "2rem" }}
+            />
+            <TextField
+              fullWidth
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              sx={{ marginBottom: "1rem" }}
+            />
+
+            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+              <Link
+                to="/login"
+                style={{ textDecoration: "none", marginBottom: "1rem" }}
+              >
+                login
+              </Link>
+              <Link
+                to="/forgetpassword"
+                style={{ textDecoration: "none", marginBottom: "1rem" }}
+              >
+                forget Password
+              </Link>
+            </Box>
+            <Button variant="contained" color="primary" fullWidth>
+             Register
+            </Button>
+          </Box>
+        </Box>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
